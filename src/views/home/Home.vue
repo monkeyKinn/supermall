47196cf20e498cbec144f2a4eadf8beb340b594a
jinsc <jinshengcong@163.com>
2020-05-29 10:33:06 +0800

    请求首页多个数据

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -8,9 +8,29 @@
 
 <script>
   import Navbar from "components/common/navbar/Navbar";
+  // 因为没有用default导出所以这里要用大括号
+  import {getHomeMultidata} from "network/home";
+
   export default {
     name: "Home",
-    components: {Navbar}
+    components: {
+      Navbar
+    },
+    data() {
+      return {
+        // 保存getHomeMultidata的res
+        banners: [],
+        recommends: []
+      }
+    },
+    created() {
+      // 组件刚加载完就发送网络请求,请求首页多个展示数据,加个()表示调用函数
+      getHomeMultidata().then(res => {
+        console.log(res);
+        this.banners = res.data.banner.list
+        this.recommends = res.data.recommend.list
+      })
+    }
   }
 </script>
 
