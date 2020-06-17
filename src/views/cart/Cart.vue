c9eac4a775fd00c6278ae299354bb04c9749d8dc
jinsc <jinshengcong@163.com>
2020-06-17 17:40:55 +0800

    购物车导航实现

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,13 +1,39 @@
 <template>
-  <h2>购物车</h2>
+  <div class="cart">
+    <navbar class="navBar">
+      <div slot="center">购物车({{length}})</div>
+    </navbar>
+  </div>
 </template>
 
 <script>
+  import Navbar from "components/common/navbar/Navbar";
+  import {mapGetters} from 'vuex'
   export default {
-    name: "Cart"
+    name: "Cart",
+    components: {Navbar},
+    data() {
+      return {
+
+      }
+    },
+    computed: {
+      // 两种语法
+      // 默认名字 用数组
+      // ...mapGetters(['cartLength','cartList'])
+      // 自定义名字 用对象
+      ...mapGetters({
+        // 对应的就是length了
+        length :'cartLength',
+        list: 'cartList'
+      })
+    }
   }
 </script>
 
 <style scoped>
-
+  .navBar {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
