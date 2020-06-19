45215db327edaff0bdf68c81d8c9cf8d5734b9e8
jinsc <jinshengcong@163.com>
2020-06-19 17:47:22 +0800

    fastclick解决移动端300ms延迟

--- src/main.js
+++ src/main.js
@@ -6,12 +6,17 @@ import toast from 'components/common/toast'
 // 挂载路由
 import router from './router'
 
+import FastClick from 'fastclick'
+
 Vue.config.productionTip = false
 
 // 定义事件总线为一个vue实例,这样才能发送事件
 Vue.prototype.$bus = new Vue();
 // 安装toast插件
 Vue.use(toast)
+
+// 解决移动端的300ms延迟
+FastClick.attach(document.body)
 
 new Vue({
   render: h => h(App),
