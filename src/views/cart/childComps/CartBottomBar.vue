2d721e357775ca971f2053bf31b10419ece66b07
jinsc <jinshengcong@163.com>
2020-06-19 15:39:11 +0800

    全选按钮点击效果

--- src/views/cart/childComps/CartBottomBar.vue
+++ src/views/cart/childComps/CartBottomBar.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="bottom-bar">
     <div class="checkAll">
-      <check-button :is-checked="isSelectAll" class="check-button"/>
+      <check-button :is-checked="isSelectAll"
+                    class="check-button"
+                    @click.native="selAllClick"/>
       <span>全选</span>
     </div>
 
@@ -41,7 +43,19 @@
       isSelectAll() {
         // return !(this.cartList.filter(item => !item.checked).length)
         return (this.cartList.length === 0) ? false :
-                !this.cartList.find(item => !item.checked)
+            !this.cartList.find(item => !item.checked)
+      }
+    },
+    methods: {
+      selAllClick() {
+        // if (this.isSelectAll) { //全部选中
+        //   this.cartList.forEach(item => item.checked = false)
+        // } else { //全部不选中或者部分选中
+        //   this.cartList.forEach(item => item.checked = true)
+        // }
+        this.isSelectAll ?
+            this.cartList.forEach(item => item.checked = false) :
+            this.cartList.forEach(item => item.checked = true)
       }
     }
   }
